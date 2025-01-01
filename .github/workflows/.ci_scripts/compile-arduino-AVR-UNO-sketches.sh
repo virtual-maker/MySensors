0e98c3b7325189226e108ae89c6980798006f41f
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2025-01-01 11:10:19 +0100

    Update compile script and build config for flexibility
    Modified `compile-arduino-AVR-UNO-sketches.sh`
    to accept FQBN, sketches directory, and blacklist file
    as arguments, replacing hardcoded values.

--- .github/workflows/.ci_scripts/compile-arduino-AVR-UNO-sketches.sh
+++ .github/workflows/.ci_scripts/compile-arduino-AVR-UNO-sketches.sh
@@ -1,24 +1,39 @@
 #!/bin/bash
 
+# Fully Qualified Board Name (FQBN) of the board to compile for
+FQBN="$1" # "arduino:avr:uno"
+# path to the sketches directory
+SKETCHES="$2" # "./examples"
 # Define the path to the blacklist file
-EXCLUDES_FILE="./.github/workflows/.ci_scripts/Arduino-AVR-UNO-excludes"
+EXCLUDES="$3" # "./.github/workflows/.ci_scripts/Arduino-AVR-UNO-excludes"
 
 # Read the blacklist file into an array
-if [ -f "$EXCLUDES_FILE" ]; then
-  mapfile -t excludes < "$EXCLUDES_FILE"
+if [ -f "$EXCLUDES" ]; then
+  mapfile -t excludes < "$EXCLUDES"
 else
   excludes=()
 fi
 
-echo "Excluded files:"
-echo "${excludes[@]}"
-echo "<END>"
+#### Debugging output
+#echo "Excluded files:"
+#echo "${excludes[@]}"
+#echo "<END>"
 
 # Find all .ino files and compile each one, excluding those in the blacklist
-find ./examples -name "*.ino" | while read sketch; do
+#find ./examples -name "*.ino" | while read sketch; do
+#  if [[ ! " ${excludes[@]} " =~ " ${sketch} " ]]; then
+#    echo "Compiling $sketch"
+#    arduino-cli compile --fqbn arduino:avr:uno "$sketch"
+#  else
+#    echo "Skipping $sketch (blacklisted)"
+#  fi
+#done
+
+# Find all .ino files and compile each one, excluding those in the blacklist
+find "$SKETCHES" -name "*.ino" | while read sketch; do
   if [[ ! " ${excludes[@]} " =~ " ${sketch} " ]]; then
     echo "Compiling $sketch"
-    arduino-cli compile --fqbn arduino:avr:uno "$sketch"
+    arduino-cli compile --fqbn "$FQBN" "$sketch"
   else
     echo "Skipping $sketch (blacklisted)"
   fi
