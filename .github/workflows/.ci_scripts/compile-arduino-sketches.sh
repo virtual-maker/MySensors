9a4480885b7c4a91dd9dd99584a004c77f4b754a
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2025-01-01 20:03:47 +0100

    Uncomment arduino-cli compile command in script

--- .github/workflows/.ci_scripts/compile-arduino-sketches.sh
+++ .github/workflows/.ci_scripts/compile-arduino-sketches.sh
@@ -26,7 +26,7 @@ echo "<END>"
 find "$SKETCHES" -name "*.ino" | while read sketch; do
   if ! grep -Fxq "$sketch" "$BLACKLIST_FILE"; then
     echo "Compiling $sketch"
-    #arduino-cli compile --fqbn "$FQBN" "$sketch" --warnings more
+    arduino-cli compile --fqbn "$FQBN" "$sketch" --warnings more
 		compile_result=$?
     if [ $compile_result -ne 0 ]; then
       result=1
