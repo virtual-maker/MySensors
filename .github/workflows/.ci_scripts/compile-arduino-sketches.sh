17eb06e12e8db0592b91792c8a9680190cd8d78b
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2025-01-01 14:14:46 +0100

    Refactor blacklist handling in script
    The script now creates or overwrites
    a `blacklist.txt` file to store blacklist entries.

--- .github/workflows/.ci_scripts/compile-arduino-sketches.sh
+++ .github/workflows/.ci_scripts/compile-arduino-sketches.sh
@@ -2,27 +2,28 @@
 
 # Fully Qualified Board Name (FQBN) of the board to compile for
 FQBN="$1" # "arduino:avr:uno"
-# path to the sketches directory
+# Path to the sketches directory
 SKETCHES="$2" # "./examples"
 # Define the path to the blacklist file
 EXCLUDES="$3" # "./.github/workflows/.ci_scripts/Arduino-AVR-UNO-excludes"
 
-# Read the blacklist file into an array
+# Create or overwrite the blacklist.txt file
+BLACKLIST_FILE="./blacklist.txt"
+> "$BLACKLIST_FILE"
+
+# Read the blacklist file and write to blacklist.txt, ignoring lines starting with # or are whitespace only
 if [ -f "$EXCLUDES" ]; then
-  #mapfile -t excludes < "$EXCLUDES"
-	mapfile -t excludes < <(grep -vE '^\s*#|^\s*$' "$EXCLUDES")
-else
-  excludes=()
+  grep -vE '^\s*#|^\s*$' "$EXCLUDES" > "$BLACKLIST_FILE"
 fi
 
 #### Debugging output
 echo "Excluded files:"
-echo "${excludes[@]}"
+cat "$BLACKLIST_FILE"
 echo "<END>"
 
 # Find all .ino files and compile each one, excluding those in the blacklist
 find "$SKETCHES" -name "*.ino" | while read sketch; do
-  if [[ ! " ${excludes[@]} " =~ " ${sketch} " ]]; then
+  if ! grep -Fxq "$sketch" "$BLACKLIST_FILE"; then
     echo "Compiling $sketch"
     arduino-cli compile --fqbn "$FQBN" "$sketch" --warnings more
   else
