513d4caa7e383ec6dd844186beedc258437beb29
Virtual Maker <56232456+virtual-maker@users.noreply.github.com>
2025-01-01 01:46:04 +0100

    Update build process to compile all example sketches
    for Arduino UNO

---
+++ .github/workflows/.ci_scripts/compile-arduino-AVR-UNO-sketches.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Read the blacklist file into an array
+if [ -f Arduino-AVR-excludes ]; then
+  mapfile -t excludes < Arduino-AVR-UNO-excludes
+else
+  excludes=()
+fi
+
+# Find all .ino files and compile each one, excluding those in the blacklist
+find ./examples -name "*.ino" | while read sketch; do
+  if [[ ! " ${excludes[@]} " =~ " ${sketch} " ]]; then
+    echo "Compiling $sketch"
+    arduino-cli compile --fqbn arduino:avr:uno "$sketch"
+  else
+    echo "Skipping $sketch (blacklisted)"
+  fi
+done
